c80766651a043b87b4f172d3958ab3527553f552
zazedd <leomendesantos@gmail.com>
2024-09-21 00:41:36 +0100

    better log

--- modules/minecraft-servers.nix
+++ modules/minecraft-servers.nix
@@ -421,7 +421,7 @@ in
           startScript = pkgs.writeScript "minecraft-start-${name}" ''
             #!${pkgs.runtimeShell}
             echo "IM STARTING"
-            ${tmux} -S ${tmuxSock} new -d ${getExe conf.package} ${conf.jvmOpts} &> /home/zazed/tmuxlog.log
+            ${tmux} -S ${tmuxSock} new -d ${getExe conf.package} ${conf.jvmOpts} &> /run/zazed/tmuxlog.log
             echo "ALL GOOD"
 
             # HACK: PrivateUsers makes every user besides root/minecraft `nobody`, so this restores old tmux behavior
